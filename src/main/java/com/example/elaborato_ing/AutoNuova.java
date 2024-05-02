1f179aefdf6da871435a46f934b2c9de8cb0c3aa
Davide <dlosco03@gmail.com>
2024-05-02 12:56:29 +0200

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/example/elaborato_ing/AutoNuova.java
+++ src/main/java/com/example/elaborato_ing/AutoNuova.java
@@ -8,4 +8,8 @@ public class AutoNuova extends Auto{
     private String descrione;
     private double costo;
     private int sconto;
+
+    public AutoNuova(String marca, String modello, double lunghezza, double altezza, double larghezza, double peso, double volumeBagagliaio, List<Image> immagine, Motore motore, List<Optional> optionals) {
+        super(marca, modello, lunghezza, altezza, larghezza, peso, volumeBagagliaio, immagine, motore, optionals);
+    }
 }
