dec296a84bfc2c00b05f56fa859772fc729aaddc
rockg <rock.gladietor03@gmail.com>
2024-05-04 15:58:48 +0200

    uiiguiuhu

--- src/main/java/com/example/elaborato_ing/AutoNuova.java
+++ src/main/java/com/example/elaborato_ing/AutoNuova.java
@@ -9,7 +9,7 @@ public class AutoNuova extends Auto{
     private double costo;
     private int sconto;
 
-    public AutoNuova(Marca marca, Modello modello, double lunghezza, double altezza, double larghezza, double peso, double volumeBagagliaio, Motore motore, int costo, String sconto,List<String> colori) {
-        super(marca, modello, lunghezza, altezza, larghezza, peso, volumeBagagliaio, motore, costo, sconto,colori);
+    public AutoNuova(Marca marca, Modello modello, double lunghezza, double altezza, double larghezza, double peso, double volumeBagagliaio, Motore motore, int costo/*, String sconto,List<String> colori*/) {
+        super(marca, modello, lunghezza, altezza, larghezza, peso, volumeBagagliaio, motore, costo, sconto/*,colori*/);
     }
 }
--- src/main/java/com/example/elaborato_ing/AutoUsata.java
+++ src/main/java/com/example/elaborato_ing/AutoUsata.java
@@ -12,7 +12,7 @@ public class AutoUsata extends Auto{
     private int incidenti;
     private Stato interni;
 
-    public AutoUsata(Marca marca, Modello modello, double lunghezza, double altezza, double larghezza, double peso, double volumeBagagliaio, Motore motore, int costo, String sconto, List<String> colori) {
-        super(marca, modello, lunghezza, altezza, larghezza, peso, volumeBagagliaio, motore, costo, sconto, colori );
+    public AutoUsata(Marca marca, Modello modello, double lunghezza, double altezza, double larghezza, double peso, double volumeBagagliaio, Motore motore, int costo, String sconto/*, List<String> colori*/) {
+        super(marca, modello, lunghezza, altezza, larghezza, peso, volumeBagagliaio, motore, costo, sconto/*, colori*/ );
     }
 }
