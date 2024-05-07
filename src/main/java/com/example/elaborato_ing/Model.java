066ce7a9853c9bf5847ea6d5706df62f060c5ae6
Davide <dlosco03@gmail.com>
2024-05-07 10:47:06 +0200

    Merge remote-tracking branch 'origin/main'

---
+++ src/main/java/com/example/elaborato_ing/Model.java
@@ -0,0 +1,12 @@
+package com.example.elaborato_ing;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Model {
+
+
+}
