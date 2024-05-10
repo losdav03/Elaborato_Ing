5e895a8ff9b6b27ccbc2654c39f4672e903c3db7
rockg <rock.gladietor03@gmail.com>
2024-05-10 12:37:41 +0200

    uiiguiuhu

---
+++ src/main/java/com/example/elaborato_ing/RiepilogoController.java
@@ -0,0 +1,10 @@
+package com.example.elaborato_ing;
+
+import javafx.fxml.FXML;
+
+import javax.swing.text.html.ListView;
+
+public class RiepilogoController {
+    @FXML
+    private ListView<String> preventiviListView;
+}
