3f358275fd1e4e67af43b5c89f996f27feb6ebd0
rockg <rock.gladietor03@gmail.com>
2024-05-10 13:56:04 +0200

    uiiguiuhu

--- src/main/java/com/example/elaborato_ing/AvvisaController.java
+++ src/main/java/com/example/elaborato_ing/AvvisaController.java
@@ -1,5 +1,11 @@
 package com.example.elaborato_ing;
 
+import javafx.event.ActionEvent;
+
 public class AvvisaController {
 
+    Model model = new Model();
+    public void Avvisa(ActionEvent actionEvent) {
+
+    }
 }
--- src/main/java/com/example/elaborato_ing/ValutaController.java
+++ src/main/java/com/example/elaborato_ing/ValutaController.java
@@ -1,5 +1,11 @@
 package com.example.elaborato_ing;
 
+import javafx.event.ActionEvent;
+
 public class ValutaController {
 
+    Model model = new Model();
+    public void Valuta(ActionEvent actionEvent) {
+
+    }
 }
