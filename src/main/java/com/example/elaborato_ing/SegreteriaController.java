d67f83dccb2ba59cc795dbc98143abb3695a7d29
rockg <rock.gladietor03@gmail.com>
2024-05-11 17:26:10 +0200

    uiiguiuhu

---
+++ src/main/java/com/example/elaborato_ing/SegreteriaController.java
@@ -0,0 +1,17 @@
+package com.example.elaborato_ing;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SegreteriaController {
+
+}
