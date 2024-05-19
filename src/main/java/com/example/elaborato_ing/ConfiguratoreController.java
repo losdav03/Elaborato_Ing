af010351d0ea69f01e1fc444d32730e31eda252b
rockg <rock.gladietor03@gmail.com>
2024-05-19 17:26:47 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/example/elaborato_ing/Model.java
    #	src/main/java/com/example/elaborato_ing/ValutaController.java
    #	src/main/resources/com/example/elaborato_ing/TXT/Preventivi.txt

--- src/main/java/com/example/elaborato_ing/ConfiguratoreController.java
+++ src/main/java/com/example/elaborato_ing/ConfiguratoreController.java
@@ -13,10 +13,15 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -241,7 +246,61 @@ public class ConfiguratoreController {
 
     @FXML
     public void generaPDF() {
+        try {
+            // Creazione del documento PDF
+            PDDocument document = new PDDocument();
+            PDPage page = new PDPage();
+            document.addPage(page);
+            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+            //contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
+            contentStream.beginText();
+            contentStream.newLineAtOffset(100, 700);
 
+            // Scrittura dei dati dell'auto nel PDF
+            contentStream.showText("Dati dell'auto:");
+            contentStream.newLine();
+            contentStream.showText("Marca: " + marca.getValue());
+            contentStream.newLine();
+            contentStream.showText("Modello: " + modello.getValue());
+            contentStream.newLine();
+            contentStream.showText("Altezza: " + altezza.getText());
+            contentStream.newLine();
+            contentStream.showText("Lunghezza: " + lunghezza.getText());
+            contentStream.newLine();
+            contentStream.showText("Larghezza: " + larghezza.getText());
+            contentStream.newLine();
+            contentStream.showText("Peso: " + peso.getText());
+            contentStream.newLine();
+            contentStream.showText("Volume: " + volume.getText());
+            contentStream.newLine();
+            contentStream.showText("Alimentazione: " + alimentazione.getText());
+            contentStream.newLine();
+            contentStream.showText("Motore: " + motore.getText());
+            contentStream.newLine();
+            contentStream.showText("Prezzo: " + prezzo.getText());
+            contentStream.endText();
+
+            // Chiusura del content stream
+            contentStream.close();
+
+            // Salvataggio del PDF
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF files (*.pdf)", "*.pdf"));
+            fileChooser.setInitialFileName("auto_info.pdf");
+            File file = fileChooser.showSaveDialog(new Stage());
+            if (file != null) {
+                document.save(file);
+                document.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Gestione degli errori durante la creazione del PDF
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Errore");
+            alert.setHeaderText(null);
+            alert.setContentText("Si è verificato un errore durante la generazione del PDF.");
+            alert.showAndWait();
+        }
     }
 
     @FXML
